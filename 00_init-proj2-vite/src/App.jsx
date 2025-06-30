37f53b382957bea1af2f7e70ac2f2698b56391d0
wisdomm11 <gurojihoon@gmail.com>
2025-06-30 15:01:33 +0900

    11

---
+++ 00_init-proj2-vite/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      초기 세팅 완료
+    </>
+  )
+}
+
+export default App
---
+++ init-proj2-vite-test2/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        초기 세팅 완료
+      </div>
+      
+    </>
+  )
+}
+
+export default App
